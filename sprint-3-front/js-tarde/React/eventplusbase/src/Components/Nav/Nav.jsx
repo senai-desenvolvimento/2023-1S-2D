533b25d8548fd9d18155d4daf624a7a62170377a
Eduardo Costa <eduardo.costa@docente.senai.br>
2023-11-06 16:57:37 -0300

    Adiciona o nav do projeto ok

--- sprint-3-front/js-tarde/React/eventplusbase/src/Components/Nav/Nav.jsx
+++ sprint-3-front/js-tarde/React/eventplusbase/src/Components/Nav/Nav.jsx
@@ -25,9 +25,9 @@ const Nav = ( { setExibeNavbar, exibeNavbar } ) => {
 
             <div className='navbar__items-box'>
                 <Link to="/" className='navbar__item'>Home</Link>
-                <Link to="/tipo-eventos">Tipo Eventos</Link>
-                <Link to="/eventos">Eventos</Link>
-                <Link to="/login">Login</Link>
+                <Link to="/tipo-eventos" className='navbar__item'>Tipo Eventos</Link>
+                <Link to="/eventos" className='navbar__item'>Eventos</Link>
+                <Link to="/login" className='navbar__item'>Login</Link>
             </div>
         </nav>
     );
